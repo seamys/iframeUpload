bb568005472aeb598a7fdc41e281b497845d9173
seamys <seamys@foxmail.com>
2016-04-06 16:36:29 +0800

    Update source

--- src/ngiframeUpload.js
+++ src/ngiframeUpload.js
@@ -1,33 +1,34 @@
 (function () {
     'use-strict';
-
     var defaults = {
-        templateUrl: 'ng-iframeUpload.html'
+        templateUrl: 'ng-uploader.html'
     };
+    var templateUrl = defaults.template_url;
     var iframeUpload = function ($templateCache) {
-        $templateCache.put(defaults.template_url,
-            '<iframe width="0" height="0" id="{{iframeName}}" name="{{iframeName}}"></iframe>' +
+
+        $templateCache.put(templateUrl,
+            '<iframe width="0" height="0" id="{{iframeName}}" name="{{iframeName}}" ng-change="iframeChange()" ></iframe>' +
             '<form style="display: none" action="{{url}}" method="post" enctype="multipart/form-data" target="{{iframeName}}">' +
-            '<input type="hidden" ng-repeat="item in hiddens" name="{{item.key}}" value="{{item.value}}" /></form>');
+            '<input type="file" ng-change="fileChange()" name="file" />' +
+            '<input type="hidden" ng-repeat="item in hiddens" name="{{item.key}}" value="{{item.value}}" />' +
+            '</form>');
+
         return {
             restrict: 'EA',
             scope: {
-                images: '=',
-                thumbsNum: '@'
+                url: '@',
+                callback: '&'
             },
-            controller: [
-                '$scope',
-                function ($scope) {
-                    $scope.$on('ng-change', function (e, args) {
-                        $scope.openGallery(args.index);
-                    });
-                }
-            ],
+            replace: true,
             templateUrl: function (element, attrs) {
                 return attrs.templateUrl || defaults.templateUrl;
+            },
+            link: function (scope, element, attrs) {
+                console.log(arguments);
             }
         }
     };
-    angular.module('iframeUpload', []).directive('iframeUpload', iframeUpload);
+    angular.module('ngUtils', []).directive('ngUploader', iframeUpload);
     iframeUpload.$inject = ['$templateCache'];
+    console.log(123);
 })();
